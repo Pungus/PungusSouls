cfe36b8492bb399cf2e29c9b36aa7205fc3c1e5c
Jeremy <Jeremy.w.keating@gmail.com>
2026-07-04 00:49:01 +1200

    stuff

--- EnvironmentManager.cs
+++ EnvironmentManager.cs
@@ -1,64 +1,51 @@
 ﻿using HarmonyLib;
 using UnityEngine;
 
-public class EnvironmentManager
+namespace PungusSouls
 {
-    [HarmonyPatch(typeof(EnvMan), "Awake")]
-    private static class EnvMan_Awake_Patches
+    public class EnvironmentManager
     {
-        public static void Postfix(EnvMan __instance)
+        public static AudioClip firelinkClip;
+        private static bool initialized;
+
+        [HarmonyPatch(typeof(EnvMan), "Awake")]
+        private static class EnvMan_Awake_Patch
         {
-            __instance.m_environments.Add(newEnv);
-            if (clearCloudsCopy == null)
+            static void Postfix(EnvMan __instance)
             {
-                foreach (EnvSetup environment in __instance.m_environments)
+                if (initialized)
+                    return;
+
+                initialized = true;
+
+                if (firelinkClip == null)
                 {
-                    if (environment.m_name == "Clear")
+                    firelinkClip = PungusSoulsPlugin.assetBundle.LoadAsset<AudioClip>("FirelinkShrine");
+                }
+
+                EnvSetup newEnv = new EnvSetup
+                {
+                    m_name = "FirelinkShrine",
+                    m_ambientLoop = firelinkClip
+                };
+
+                for (int i = 0; i < __instance.m_environments.Count; i++)
+                {
+                    var env = __instance.m_environments[i];
+
+                    if (env.m_name == "Crypt")
                     {
-                        clearCloudsCopy = Object.Instantiate(environment.m_psystems[0]);
-                        newEnv.m_psystems = new GameObject[1] { clearCloudsCopy };
-                        newEnv.m_sunColorMorning = environment.m_sunColorDay;
-                        newEnv.m_sunColorDay = environment.m_sunColorDay;
-                        newEnv.m_sunColorEvening = environment.m_sunColorDay;
-                        newEnv.m_sunColorEvening = environment.m_sunColorDay;
-                        newEnv.m_sunColorNight = environment.m_sunColorDay;
-                        newEnv.m_lightIntensityDay = environment.m_lightIntensityDay;
-                        newEnv.m_lightIntensityNight = environment.m_lightIntensityDay;
-                        newEnv.m_sunAngle = environment.m_sunAngle;
-                        newEnv.m_ambColorDay = environment.m_ambColorDay;
-                        newEnv.m_ambColorNight = environment.m_ambColorDay;
+                        newEnv.m_musicDay = env.m_musicDay;
+                        newEnv.m_musicEvening = env.m_musicEvening;
+                        newEnv.m_musicMorning = env.m_musicMorning;
+                        newEnv.m_musicNight = env.m_musicNight;
+                        break;
                     }
                 }
+
+                __instance.m_environments.Add(newEnv);
+                __instance.InitializeEnvironment(newEnv);
             }
-            foreach (EnvSetup environment2 in __instance.m_environments)
-            {
-                if (environment2.m_name == "Crypt")
-                {
-                    newEnv.m_ambientLoop = environment2.m_ambientLoop;
-                    newEnv.m_musicDay = environment2.m_musicDay;
-                    newEnv.m_musicEvening = environment2.m_musicEvening;
-                    newEnv.m_musicMorning = environment2.m_musicMorning;
-                    newEnv.m_musicNight = environment2.m_musicNight;
-                    newEnv.m_ambientLoop = environment2.m_ambientLoop;
-                }
-            }
-            __instance.InitializeEnvironment(newEnv);
-            InitEnvSetup();
         }
-    }
-
-    public static EnvSetup newEnv = new EnvSetup();
-
-    public static GameObject clearCloudsCopy;
-
-    public static AudioClip musicCopy;
-
-    public static void InitEnvSetup()
-    {
-        newEnv.m_name = "FirelinkShrine";
-        newEnv.m_fogDensityMorning = 0f;
-        newEnv.m_fogDensityDay = 0f;
-        newEnv.m_fogDensityEvening = 0f;
-        newEnv.m_fogDensityNight = 0f;
     }
 }
